27570461736ef88567ba6b894516ce3c7c4771fe
syedrameezahmed18 <noir.ramsam@gmail.com>
2020-12-14 11:42:36 +0500

    new footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,11 @@
 import React,{useContext,useState,Fragment} from 'react';
-import gh from '../images/github.png';
+import gh from '../images/newgh.png';
 import wa from '../images/neww.png';
 import email from '../images/gmail.png'
 import {Screencontext} from '../context.js'
+import { GrFacebook } from 'react-icons/gr';
+import linkedin from '../images/linkedin.png';
+import fb from '../images/facebook.png';
 
 const Footer = () => {
 
@@ -10,16 +13,40 @@ const Footer = () => {
     return(
 
         <div className="footer">
-            <div className="upperfooter">
-                <div className="upperleft">
-                    {   isscreen > 800 ? (
+            <div className="toplayer">
+                <img src={wa} title="whatsapp"/>
+                <img src={linkedin} title="LinkedIn" />
+                <img src={fb} title="facebook" />
+                <img src={email} title="gmail" />
+                <img src={gh} title="github" />
+            </div>
+            <div className="topmiddle">
+                <p className="justset">About</p>
+                <p>Services</p>
+                <p>Skills</p>
+                <p>Contact</p>
+            </div>
+            <div className="middlelayer">
+            {   isscreen > 800 ? (
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3617.271795157782!2d67.11231761447952!3d24.95686574753434!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3eb3475ef3e7018d%3A0x322eef59855cccf!2sGulshan-e-kaneez%20Fatima%20block%204!5e0!3m2!1sen!2s!4v1606401972963!5m2!1sen!2s" 
-                    width="500" 
-                    height="220" frameborder="0"  allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>):
+                    width="800" 
+                    height="280" frameborder="0"  allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>):
                     (<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3617.271795157782!2d67.11231761447952!3d24.95686574753434!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3eb3475ef3e7018d%3A0x322eef59855cccf!2sGulshan-e-kaneez%20Fatima%20block%204!5e0!3m2!1sen!2s!4v1606401972963!5m2!1sen!2s" 
-                    width="200" 
-                    height="140" frameborder="0"  allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>)
+                    width="300" 
+                    height="160" frameborder="0"  allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>)
                     }
+            </div>
+            <div className="lowerfooter">
+                <p>Syed Rameez Ahmed Portfolio &copy; 2020</p>
+                </div>
+        </div>)
+}
+
+export default Footer;
+
+{/*<div className="upperfooter">
+                <div className="upperleft">
+                    
                 </div>
                 <div className="upperright">
                     <h3>Contact Me</h3>
@@ -37,10 +64,4 @@ const Footer = () => {
                     </div>
                 </div>
             </div>
-            <div className="lowerfooter">
-                <p>Syed Rameez Ahmed Portfolio &copy; 2020</p>
-            </div>
-        </div>)
-}
-
-export default Footer;
+            */}
